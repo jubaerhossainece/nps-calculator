b3ade90fada3b255762f92e12c58f26a00c6bb01
arafat.r.riseuplabs <arafat.r.riseuplabs@gmail.com>
2023-06-11 16:26:22 +0600

    api modify add id to search

--- app/Http/Resources/FeedbackResource.php
+++ app/Http/Resources/FeedbackResource.php
@@ -17,6 +17,7 @@ class FeedbackResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email,
             'rating' => $this->rating,
